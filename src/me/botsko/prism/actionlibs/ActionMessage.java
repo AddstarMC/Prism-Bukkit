8adfde182799fb54df433469ca5a961b5d1d4aaa
Mike Botsko <botsko@gmail.com>
2013-01-06 20:32:19 -0800

    Actions without a handler don't show a nice name for an object in the message, since they have none

--- src/me/botsko/prism/actionlibs/ActionMessage.java
+++ src/me/botsko/prism/actionlibs/ActionMessage.java
@@ -44,7 +44,9 @@ public class ActionMessage {
 		msg += getPosNegPrefix();
 		msg += highlight + a.getPlayer_name();
 		msg += " " + ChatColor.WHITE + a.getType().getNiceDescription();
-		msg += " " + highlight + a.getNiceName();
+		if(a.getType().getHandler() != null){
+			msg += " " + highlight + a.getNiceName();
+		}
 		msg += " " + ChatColor.GRAY + "(a:" + a.getType().getActionShortType() + ")";
 		
 		return msg;
