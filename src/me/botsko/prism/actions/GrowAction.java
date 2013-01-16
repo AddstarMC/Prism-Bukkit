c57e935f9dfaef4c011d933282a864d8289a9b3e
Mike Botsko <botsko@gmail.com>
2013-01-15 18:35:18 -0800

    Making grow extend blockaction

--- src/me/botsko/prism/actions/GrowAction.java
+++ src/me/botsko/prism/actions/GrowAction.java
@@ -1,19 +1,8 @@
 package me.botsko.prism.actions;
 
 import org.bukkit.block.BlockState;
-import org.bukkit.inventory.ItemStack;
 
-public class GrowAction extends GenericAction {
-	
-	/**
-	 * 
-	 */
-	protected BlockState blockstate;
-	
-	/**
-	 * 
-	 */
-	protected BlockActionData actionData;
+public class GrowAction extends BlockAction {
 	
 	
 	/**
@@ -24,13 +13,12 @@ public class GrowAction extends GenericAction {
 	 */
 	public GrowAction( ActionType action_type, BlockState blockstate, String player ){
 		
-		super(action_type, player);
+		super(action_type, null, player);
 		
 		// Build an object for the specific details of this action
 		actionData = new BlockActionData();
 
 		if(blockstate != null){
-			this.blockstate = blockstate;
 			actionData.block_id = blockstate.getTypeId();
 			actionData.block_subid = blockstate.getData().getData();
 			this.world_name = blockstate.getWorld().getName();
@@ -43,60 +31,5 @@ public class GrowAction extends GenericAction {
 		setDataFromObject();
 		setObjectFromData();
 		
-	}
-	
-	
-	/**
-	 * 
-	 */
-	public void setData( String data ){
-		this.data = data;
-		setObjectFromData();
-	}
-	
-	
-	/**
-	 * 
-	 */
-	protected void setDataFromObject(){
-		data = gson.toJson(actionData);
-	}
-	
-	
-	/**
-	 * 
-	 */
-	protected void setObjectFromData(){
-		if(data != null){
-			actionData = gson.fromJson(data, BlockActionData.class);
-		}
-	}
-	
-	
-	/**
-	 * 
-	 */
-	public int getBlock_id(){
-		return actionData.block_id;
-	}
-	
-	
-	/**
-	 * 
-	 */
-	public byte getBlock_subid(){
-		return actionData.block_subid;
-	}
-	
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public String getNiceName(){
-		String name = "block";
-		ItemStack i = new ItemStack( getBlock_id(),getBlock_subid());
-		name = i.getType().name().toLowerCase().replace("_", " ");
-		return name;
 	}
 }
