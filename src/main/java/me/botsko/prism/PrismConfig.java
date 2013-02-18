7582de3d9df6e52ea318fce182f02afe96534f4f
Mike Botsko <botsko@gmail.com>
2013-02-18 08:54:57 -0800

    Splitting config class into a base/specific so we can re-use the base in addons

--- src/main/java/me/botsko/prism/PrismConfig.java
+++ src/main/java/me/botsko/prism/PrismConfig.java
@@ -1,40 +1,30 @@
 package me.botsko.prism;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
 
-public class PrismConfig {
-	
-	/**
-	 * 
-	 */
-	protected Prism plugin;
-	
+public class PrismConfig extends ConfigBase {
+
 	
 	/**
 	 * 
 	 * @param plugin
 	 */
-	public PrismConfig( Prism plugin ) {
-		this.plugin = plugin;
+	public PrismConfig(Plugin plugin) {
+		super(plugin);
 	}
 	
-	
+
 	/**
 	 * 
 	 * @param plugin
 	 */
 	public FileConfiguration getConfig(){
 		
-		FileConfiguration config = plugin.getConfig();
+		config = plugin.getConfig();
 		
 		// set defaults
 		config.addDefault("prism.debug", false);
@@ -195,102 +185,5 @@ public class PrismConfig {
 		
 		return config;
 		
-	}
-	
-	
-	/**
-	 * Loads language configuration
-	 * @return
-	 */
-	public FileConfiguration getLang(){
-
-		String lang_file = plugin.config.getString("prism.language");
-		if(lang_file == null){
-			lang_file = "en-us";
-		}
-		
-		// Read the base config
-		FileConfiguration config = loadConfig( "languages/", lang_file );
-		return config;
-		
-	}
-	
-	
-	/**
-	 * Loads item database
-	 * @return
-	 */
-	public FileConfiguration getItems(){
-		FileConfiguration config = loadConfig( "", "items" );
-		return config;
-	}
-	
-	
-	/**
-	 * Returns base directory for config
-	 * @return
-	 */
-	protected File getDirectory(){
-		File dir = new File(plugin.getDataFolder()+"");
-		return dir;
-	}
-	
-	
-	/**
-	 * Returns chosen filename with directory
-	 * @return
-	 */
-	protected File getFilename( String filename ){
-		File file = new File(getDirectory(), filename + ".yml");
-		return file;
-	}
-	
-	
-	/**
-	 * 
-	 * @param player
-	 * @return
-	 */
-	protected FileConfiguration loadConfig( String default_folder, String filename ){
-		File file = getFilename( filename );
-		if(file.exists()){
-			return YamlConfiguration.loadConfiguration(file);
-		} else {
-			// Look for defaults in the jar
-		    InputStream defConfigStream = plugin.getResource(default_folder+filename+".yml");
-		    if (defConfigStream != null) {
-		        return YamlConfiguration.loadConfiguration(defConfigStream);
-		    }
-		    return null;
-		}
-	}
-	
-	
-	/**
-	 * 
-	 * @param config
-	 */
-	protected void saveConfig( String filename, FileConfiguration config ){
-		File file = getFilename( filename );
-		try {
-			config.save(file);
-		} catch (IOException e) {
-			plugin.log("Could not save the configuration file to "+file);
-		}
-	}
-	
-	
-	/**
-	 * 
-	 */
-	protected void write( String filename, FileConfiguration config ){
-		try {
-			BufferedWriter bw = new BufferedWriter( new FileWriter( getFilename( filename ), true ) );
-			saveConfig( filename, config );
-			bw.flush();
-			bw.close();
-		} catch (IOException e){
-
-        }
 	}
 }
