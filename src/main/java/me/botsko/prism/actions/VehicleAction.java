dbdb28ca6d5eaac8d5ec538f383b352f0d57face
Mike Botsko <botsko@gmail.com>
2013-03-06 13:18:26 -0800

    Adding rollback support for broken vehicles

--- src/main/java/me/botsko/prism/actions/VehicleAction.java
+++ src/main/java/me/botsko/prism/actions/VehicleAction.java
@@ -1,5 +1,13 @@
 package me.botsko.prism.actions;
 
+import me.botsko.prism.actionlibs.QueryParameters;
+import me.botsko.prism.appliers.ChangeResult;
+import me.botsko.prism.appliers.ChangeResultType;
+
+import org.bukkit.entity.Boat;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Minecart;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.PoweredMinecart;
 import org.bukkit.entity.StorageMinecart;
 import org.bukkit.entity.Vehicle;
@@ -30,5 +38,31 @@ public class VehicleAction extends GenericAction {
 	 */
 	public String getNiceName(){
 		return this.data;
+	}
+	
+	
+	/**
+	 * 
+	 */
+	@Override
+	public ChangeResult applyRollback( Player player, QueryParameters parameters, boolean is_preview ){
+		
+		Entity vehicle = null;
+		if( this.data.equals("powered minecart") ){
+			vehicle = getWorld().spawn( getLoc(), PoweredMinecart.class );
+		}
+		else if( this.data.equals("storage minecart") ){
+			vehicle = getWorld().spawn( getLoc(), StorageMinecart.class );
+		}
+		else if( this.data.equals("minecart") ){
+			vehicle = getWorld().spawn( getLoc(), Minecart.class );
+		}
+		else if( this.data.equals("boat") ){
+			vehicle = getWorld().spawn( getLoc(), Boat.class );
+		}
+		if( vehicle != null ){
+			return new ChangeResult( ChangeResultType.APPLIED, null );
+		}
+		return new ChangeResult( ChangeResultType.SKIPPED, null );
 	}
 }
