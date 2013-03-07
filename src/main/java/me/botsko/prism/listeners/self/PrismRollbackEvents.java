68b452c9639614ac5f492dcce1fc25d9cf52f415
Mike Botsko <botsko@gmail.com>
2013-03-07 13:58:42 -0800

    Commenting out our old rollback listener, not being used atm anyway

--- src/main/java/me/botsko/prism/listeners/self/PrismRollbackEvents.java
+++ src/main/java/me/botsko/prism/listeners/self/PrismRollbackEvents.java
@@ -1,16 +1,6 @@
 package me.botsko.prism.listeners.self;
 
-import java.util.ArrayList;
-
-import me.botsko.prism.Prism;
-import me.botsko.prism.actionlibs.ActionFactory;
-import me.botsko.prism.actions.Handler;
-import me.botsko.prism.actions.PrismRollbackAction;
-import me.botsko.prism.appliers.PrismProcessType;
-import me.botsko.prism.events.BlockStateChange;
 import me.botsko.prism.events.PrismBlocksRollbackEvent;
-
-import org.bukkit.block.BlockState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -24,31 +14,31 @@ public class PrismRollbackEvents implements Listener {
 	@EventHandler
 	public void onPrismBlocksRollbackEvent(final PrismBlocksRollbackEvent event){
 
-		// Get all block changes for this event
-		ArrayList<BlockStateChange> blockStateChanges = event.getBlockStateChanges();
-		if(!blockStateChanges.isEmpty()){
-			
-			// Create an entry for the rollback as a whole
-			Handler primaryAction = ActionFactory.create("prism-process", PrismProcessType.ROLLBACK, event.onBehalfOf(), event.getCommandParams() );
-			int id = Prism.actionsRecorder.insertActionIntoDatabase( primaryAction );
-			if(id == 0){
-				return;
-			}
-			for(BlockStateChange stateChange : blockStateChanges){
-				
-				BlockState orig = stateChange.getOriginalBlock();
-				BlockState newBlock = stateChange.getNewBlock();
-
-				// Build the action
-				PrismRollbackAction action = new PrismRollbackAction("prism-rollback", orig.getTypeId(), orig.getRawData(), newBlock.getTypeId(), newBlock.getRawData(), event.onBehalfOf().getName(), id);
-				action.setWorldName(orig.getWorld().getName());
-				action.setX(orig.getX());
-				action.setY(orig.getY());
-				action.setZ(orig.getZ());
-
-				Prism.actionsRecorder.addToQueue( action );
-			}
-			Prism.actionsRecorder.save();
-		}
+//		// Get all block changes for this event
+//		ArrayList<BlockStateChange> blockStateChanges = event.getBlockStateChanges();
+//		if(!blockStateChanges.isEmpty()){
+//			
+//			// Create an entry for the rollback as a whole
+//			Handler primaryAction = ActionFactory.create("prism-process", PrismProcessType.ROLLBACK, event.onBehalfOf(), event.getCommandParams() );
+//			int id = Prism.actionsRecorder.insertActionIntoDatabase( primaryAction );
+//			if(id == 0){
+//				return;
+//			}
+//			for(BlockStateChange stateChange : blockStateChanges){
+//				
+//				BlockState orig = stateChange.getOriginalBlock();
+//				BlockState newBlock = stateChange.getNewBlock();
+//
+//				// Build the action
+//				PrismRollbackAction action = new PrismRollbackAction("prism-rollback", orig.getTypeId(), orig.getRawData(), newBlock.getTypeId(), newBlock.getRawData(), event.onBehalfOf().getName(), id);
+//				action.setWorldName(orig.getWorld().getName());
+//				action.setX(orig.getX());
+//				action.setY(orig.getY());
+//				action.setZ(orig.getZ());
+//
+//				Prism.actionsRecorder.addToQueue( action );
+//			}
+//			Prism.actionsRecorder.save();
+//		}
 	}
 }
