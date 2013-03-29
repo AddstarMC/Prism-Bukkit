4554ca54ef8d6fc03db641e9f93d9f0dd787e874
Mike Botsko <botsko@gmail.com>
2013-03-29 16:59:42 -0700

    Adding pagination next/prev commands - bug 431:resolved

--- src/main/java/me/botsko/prism/commands/PageCommand.java
+++ src/main/java/me/botsko/prism/commands/PageCommand.java
@@ -37,58 +37,78 @@ public class PageCommand implements SubHandler {
 	 */
 	public void handle(CallInfo call) {
 		
+		// Is there anything even stored to paginate?
+		String keyName = "console";
+		if( call.getSender() instanceof Player ){
+			keyName = call.getSender().getName();
+		}
+		if( !plugin.cachedQueries.containsKey( keyName ) ){
+			call.getSender().sendMessage( Prism.messenger.playerError("There's no saved query to paginate. Maybe they expired? Try your lookup again.") );
+			return;
+		}
+		
+		// Get stored results
+		QueryResult results = plugin.cachedQueries.get( keyName );
+		
 		if(call.getArgs().length != 2){
 			call.getSender().sendMessage( Prism.messenger.playerError( "Please specify a page number. Like /prism page 2" ) );
 			return;
 		}
 		
-		if(!TypeUtils.isNumeric( call.getArg(1) )){
-			call.getSender().sendMessage( Prism.messenger.playerError( "Page numbers need to actually be numbers. Like /prism page 2" ) );
-			return;
+		// Determine page number
+		int page = 1;
+		if( TypeUtils.isNumeric( call.getArg(1) ) ){
+			page = Integer.parseInt(call.getArg(1));
+		} else {
+			
+			// Next page
+			if( call.getArg(1).equals("next") || call.getArg(1).equals("n") ){
+				page = results.getPage() + 1;
+			} 
+			// Previous page
+			else if( call.getArg(1).equals("prev") || call.getArg(1).equals("p") ){
+				if( results.getPage() <= 1 ){
+					call.getSender().sendMessage( Prism.messenger.playerError( "There is no previous page." ) );
+					return;
+				}
+				page = results.getPage() - 1;
+			} else {
+				call.getSender().sendMessage( Prism.messenger.playerError( "Page numbers need to actually be numbers, or next/prev. Like /prism page 2" ) );
+				return;
+			}
 		}
 		
-		int page = Integer.parseInt(call.getArg(1));
-
+		// No negatives
 		if(page <= 0){
 			call.getSender().sendMessage( Prism.messenger.playerError("Page must be greater than zero.") );
 			return;
 		}
+
+		results.setPage(page);
 		
-		String keyName = "console";
-		if( call.getSender() instanceof Player ){
-//			Player player = call.getSender();
-			keyName = call.getSender().getName();
+		// Refresh the query time and replace
+		results.setQueryTime();
+		plugin.cachedQueries.replace( keyName, results);
+		
+		// Results?
+		if( results.getActionResults().isEmpty() ){
+			call.getSender().sendMessage( Prism.messenger.playerError( "Nothing found." + ChatColor.GRAY + " Either you're missing something, or we are." ) );
+			return;
 		}
 		
-		// Is anything even cached?
-		if(plugin.cachedQueries.containsKey( keyName )){
-			QueryResult results = plugin.cachedQueries.get( keyName );
-			results.setPage(page);
-			
-			// Refresh the query time and replace
-			results.setQueryTime();
-			plugin.cachedQueries.replace( keyName, results);
-			
-			// Results?
-			if(!results.getActionResults().isEmpty()){
-				call.getSender().sendMessage( Prism.messenger.playerHeaderMsg("Showing "+results.getTotal_results()+" results. Page "+page+" of "+results.getTotal_pages()) );
-				List<Handler> paginated = results.getPaginatedActionResults();
-				if(paginated != null){
-					for(Handler a : paginated){
-						ActionMessage am = new ActionMessage(a);
-						if( results.getParameters().allowsNoRadius() || results.getParameters().hasFlag(Flag.EXTENDED) || plugin.getConfig().getBoolean("prism.messenger.always-show-extended") ){
-							am.showExtended();
-						}
-						call.getSender().sendMessage( Prism.messenger.playerMsg( am.getMessage() ) );
-					}
-				} else {
-					call.getSender().sendMessage( Prism.messenger.playerError( "Pagination can't find anything. Do you have the right page number?" ) );
-				}
-			} else {
-				call.getSender().sendMessage( Prism.messenger.playerError( "Nothing found." + ChatColor.GRAY + " Either you're missing something, or we are." ) );
+		call.getSender().sendMessage( Prism.messenger.playerHeaderMsg("Showing "+results.getTotal_results()+" results. Page "+page+" of "+results.getTotal_pages()) );
+		List<Handler> paginated = results.getPaginatedActionResults();
+		if(paginated == null){
+			call.getSender().sendMessage( Prism.messenger.playerError( "Pagination can't find anything. Do you have the right page number?" ) );
+		}
+		
+		// Show it!
+		for(Handler a : paginated){
+			ActionMessage am = new ActionMessage(a);
+			if( results.getParameters().allowsNoRadius() || results.getParameters().hasFlag(Flag.EXTENDED) || plugin.getConfig().getBoolean("prism.messenger.always-show-extended") ){
+				am.showExtended();
 			}
-		} else {
-			call.getSender().sendMessage( Prism.messenger.playerError( "There's no lookup results to show. They may have expired." ) );
+			call.getSender().sendMessage( Prism.messenger.playerMsg( am.getMessage() ) );
 		}
 	}
 }
