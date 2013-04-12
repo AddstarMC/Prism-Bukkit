de094fbff5d88d75adf43944e14c669ac25f6fd1
Mike Botsko <botsko@gmail.com>
2013-04-11 19:40:44 -0700

    Removing unused import

--- src/main/java/me/botsko/prism/actions/ItemStackAction.java
+++ src/main/java/me/botsko/prism/actions/ItemStackAction.java
@@ -28,7 +28,6 @@ import org.bukkit.block.Jukebox;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
