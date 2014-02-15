c60c5d7bc2e0ef6caf92ed6da921797f763d886e
Mike Botsko <botsko@gmail.com>
2014-02-14 17:00:58 -0800

    Entity conditions were not properly being included in query

--- src/main/java/me/botsko/prism/database/mysql/SelectQueryBuilder.java
+++ src/main/java/me/botsko/prism/database/mysql/SelectQueryBuilder.java
@@ -105,6 +105,7 @@ public class SelectQueryBuilder extends QueryBuilder {
 		playerCondition();
 		radiusCondition();
 		blockCondition();
+		entityCondition();
 		timeCondition();
 		keywordCondition();
 		coordinateCondition();
