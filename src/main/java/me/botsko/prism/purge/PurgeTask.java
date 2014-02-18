72c28db146932cbb55f6540e253cb3b6b5923fba
Mike Botsko <botsko@gmail.com>
2014-02-17 21:02:02 -0800

    Changing how multiple purge rules are scheduled as tasks so they aren't scheduled as a simultaneous tasks. Adding /pr delete cancel - cancels all current purge tasks - bug 691:resolved

--- src/main/java/me/botsko/prism/purge/PurgeTask.java
+++ src/main/java/me/botsko/prism/purge/PurgeTask.java
@@ -63,53 +63,68 @@ public class PurgeTask implements Runnable {
 	 */
 	public void run(){
 
-    	if(paramList.size() > 0){
+    	if(paramList.isEmpty()) return;
     		
-	    	ActionsQuery aq = new ActionsQuery(plugin);
-	    	// Execute in batches so we don't tie up the db with one massive query
-	    	for( QueryParameters param : paramList ){
-	    		
-	    		boolean cycle_complete = false;
+    	ActionsQuery aq = new ActionsQuery(plugin);
+    	
+    	// Pull the next-in-line purge param
+    	QueryParameters param = paramList.get(0);
 
-	    		// We're chunking by IDs instead of using LIMIT because
-	    		// that should be a lot better as far as required record lock counts
-	    		// http://mysql.rjweb.org/doc.php/deletebig
-	    		int spread = plugin.getConfig().getInt("prism.purge.records-per-batch");
-	    		if( spread <= 1 ) spread = 10000;
-	    		int newMinId = minId + spread;
-	    		param.setMinPrimaryKey(minId);
-	    		param.setMaxPrimaryKey(newMinId);
+		boolean cycle_complete = false;
 
-				cycle_rows_affected = aq.delete(param);
-				plugin.total_records_affected += cycle_rows_affected;
+		// We're chunking by IDs instead of using LIMIT because
+		// that should be a lot better as far as required record lock counts
+		// http://mysql.rjweb.org/doc.php/deletebig
+		int spread = plugin.getConfig().getInt("prism.purge.records-per-batch");
+		if( spread <= 1 ) spread = 10000;
+		int newMinId = minId + spread;
+		param.setMinPrimaryKey(minId);
+		param.setMaxPrimaryKey(newMinId);
 
-				// If nothing (or less than the limit) has been deleted this cycle, we need to move on
-				if( newMinId > maxId ){
+		cycle_rows_affected = aq.delete(param);
+		plugin.total_records_affected += cycle_rows_affected;
 
-					// Remove rule, reset affected count, mark complete
-					paramList.remove( param );
-					cycle_complete = true;
-					
-				}
-				
-				Prism.debug("------------------- " + param.getOriginalCommand());
-				Prism.debug("newMinId: " + newMinId);
-				Prism.debug("maxId: " + maxId);
-				Prism.debug("cycle_rows_affected: " + cycle_rows_affected);
-				Prism.debug("cycle_complete: " + cycle_complete);
-				Prism.debug("plugin.total_records_affected: " + plugin.total_records_affected);
-				Prism.debug("-------------------");
+		// If nothing (or less than the limit) has been deleted this cycle, we need to move on
+		if( newMinId > maxId ){
+			// Remove rule, reset affected count, mark complete
+			paramList.remove( param );
+			cycle_complete = true;
+		}
+		
+		Prism.debug("------------------- " + param.getOriginalCommand());
+		Prism.debug("minId: " + minId);
+		Prism.debug("maxId: " + maxId);
+		Prism.debug("newMinId: " + newMinId);
+		Prism.debug("cycle_rows_affected: " + cycle_rows_affected);
+		Prism.debug("cycle_complete: " + cycle_complete);
+		Prism.debug("plugin.total_records_affected: " + plugin.total_records_affected);
+		Prism.debug("-------------------");
 
-				// Send cycle to callback
-				callback.cycle( param, cycle_rows_affected, plugin.total_records_affected, cycle_complete );
-				
-				// If cycle is incomplete, reschedule it, or reset counts
-				if( !cycle_complete ){
-					plugin.getPurgeManager().deleteTask = plugin.getServer().getScheduler().runTaskLaterAsynchronously(plugin, new PurgeTask( plugin, paramList, purge_tick_delay, newMinId, maxId, callback ), purge_tick_delay);
-				} else {
-					plugin.total_records_affected = 0;
-				}
-	    	}
-    	}
+		// Send cycle to callback
+		callback.cycle( param, cycle_rows_affected, plugin.total_records_affected, cycle_complete );
+		
+		// If cycle is incomplete, reschedule it, or reset counts
+		if( !cycle_complete ){
+			plugin.getPurgeManager().deleteTask = plugin.getServer().getScheduler().runTaskLaterAsynchronously(plugin, new PurgeTask( plugin, paramList, purge_tick_delay, newMinId, maxId, callback ), purge_tick_delay);
+		} else {
+			
+			// reset counts
+			plugin.total_records_affected = 0;
+			
+			if( paramList.isEmpty() ) return;
+			
+			Prism.log("Moving on to next purge rule...");
+			
+			// Identify the minimum for chunking
+			newMinId = PurgeChunkingUtil.getMinimumPrimaryKey();
+			if( newMinId == 0 ){
+				Prism.log("No minimum primary key could be found for purge chunking.");
+				return;
+			}
+			
+			// schedule a new task with next param
+			plugin.getPurgeManager().deleteTask = plugin.getServer().getScheduler().runTaskLaterAsynchronously(plugin, new PurgeTask( plugin, paramList, purge_tick_delay, newMinId, maxId, callback ), purge_tick_delay);
+			
+		}
 	}
 }
