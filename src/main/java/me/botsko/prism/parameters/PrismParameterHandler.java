8e6a30247f0fc5023937ccb71643af8f60f26731
Dykam <kramieb@gmail.com>
2014-02-27 02:41:04 +0100

    Rework PrismParameterHandler to be both easier yet more flexible

--- src/main/java/me/botsko/prism/parameters/PrismParameterHandler.java
+++ src/main/java/me/botsko/prism/parameters/PrismParameterHandler.java
@@ -1,22 +1,18 @@
 package me.botsko.prism.parameters;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.bukkit.command.CommandSender;
-
 import me.botsko.prism.actionlibs.QueryParameters;
+import org.bukkit.command.CommandSender;
 
 public interface PrismParameterHandler {
 	
 	public String getName();
-	
+
 	public String[] getHelp();
-	
-	public Pattern getArgumentPattern();
-	
-	public void process( QueryParameters query, Matcher input, CommandSender sender );
-	
+
+	public boolean applicable(QueryParameters query, String parameter, CommandSender sender);
+
+	public void process(QueryParameters query, String parameter, CommandSender sender);
+
 	public void defaultTo( QueryParameters query, CommandSender sender );
 
 }
