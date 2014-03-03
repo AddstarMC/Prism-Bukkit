9033675ca7f7a994ddb092fe8d040e84c2bb3ce1
Mike Botsko <botsko@gmail.com>
2014-03-02 23:12:19 -0800

    Adding new InternalAffairs service. Runs every 5 minutes to restarts recorder thread if it's stopped due to db connection errors, as long as db connection now avail.

---
+++ src/main/java/me/botsko/prism/actionlibs/InternalAffairs.java
@@ -0,0 +1,74 @@
+package me.botsko.prism.actionlibs;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import me.botsko.prism.Prism;
+
+public class InternalAffairs implements Runnable {
+	
+	private final Prism plugin;
+	
+	
+	/**
+	 * 
+	 * @param prism
+	 */
+	public InternalAffairs( Prism plugin ){
+		Prism.debug("[InternalAffairs] Keeping watch over the watchers.");
+		this.plugin = plugin;
+	}
+	
+	
+	/**
+	 * 
+	 */
+	public void run(){
+		
+		Prism.debug("INTERNAL AFFAIRS RUNNING");
+		
+		if( plugin.recordingTask != null ){
+		
+			int taskId = plugin.recordingTask.getTaskId();
+		
+			BukkitScheduler scheduler = Bukkit.getScheduler();
+		
+			// is recording task running?
+			if( scheduler.isCurrentlyRunning(taskId) || scheduler.isQueued(taskId) ){
+				Prism.debug("[InternalAffairs] Recorder is currently active. All is good.");
+				return;
+			}
+		}
+
+		Prism.log("[InternalAffairs] Recorder is NOT active... checking database");
+		
+		// is db connection valid?
+		Connection conn = null;
+		try {
+			
+			conn = Prism.dbc();
+			if( conn == null ){
+				Prism.log("[InternalAffairs] Pool returned NULL instead of a valid connection.");
+			}
+			else if( conn.isClosed() ){
+				Prism.log("[InternalAffairs] Pool returned an already closed connection.");
+			}
+			else if( conn.isValid(5) ){
+				
+				Prism.log("[InternalAffairs] Pool returned valid connection!");
+				
+				Prism.log("[InternalAffairs] Restarting scheduled recorder tasks");
+				plugin.actionRecorderTask();
+				
+			}
+		} catch (SQLException e){
+			Prism.debug("[InternalAffairs] Error: " + e.getMessage());
+	    	e.printStackTrace();
+        } finally {
+        	if(conn != null) try { conn.close(); } catch (SQLException e) {}
+        }
+	}
+}
