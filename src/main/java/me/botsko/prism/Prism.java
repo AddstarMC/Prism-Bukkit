d291326c4ef75440c98cfe9e2d4287d1ee453ff3
Mike Botsko <botsko@gmail.com>
2014-03-24 10:54:06 -0700

    Restoring internal affairs class scheduler

--- src/main/java/me/botsko/prism/Prism.java
+++ src/main/java/me/botsko/prism/Prism.java
@@ -260,6 +260,9 @@ public class Prism extends JavaPlugin {
 
 			// Delete old data based on config
 			launchScheduledPurgeManager();
+			
+			// Keep watch on db connections, other sanity
+			launchInternalAffairs();
 
 		}
 	}
@@ -1004,6 +1007,15 @@ public class Prism extends JavaPlugin {
 		// scheduledPurgeExecutor = 
 		schedulePool.scheduleAtFixedRate( purgeManager, 0, 12, TimeUnit.HOURS);
 		// scheduledPurgeExecutor.cancel();
+	}
+	
+	
+	/**
+	 * 
+	 */
+	public void launchInternalAffairs(){
+	    InternalAffairs recordingMonitor = new InternalAffairs(this);
+	    recordingMonitorTask.scheduleAtFixedRate( recordingMonitor, 0, 5, TimeUnit.MINUTES);
 	}
 
 	
