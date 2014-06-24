9c257d3da7706cd3684328c12f42527c2ce798d5
Mike Botsko <botsko@gmail.com>
2014-06-23 17:20:01 -0700

    Correcting lower half of doublechests not logging item actions. bug 906:resolved

--- src/main/java/me/botsko/prism/listeners/PrismInventoryEvents.java
+++ src/main/java/me/botsko/prism/listeners/PrismInventoryEvents.java
@@ -10,6 +10,7 @@ import me.botsko.prism.actionlibs.RecordingQueue;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.BlockState;
+import org.bukkit.block.Chest;
 import org.bukkit.block.DoubleChest;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -121,18 +122,24 @@ public class PrismInventoryEvents implements Listener {
             containerLoc = chest.getLocation();
         }
 
-        // Prism.debug("--- Inv: " + (inv == null ? "null" : inv.toString()));
-        // Prism.debug("--- InvHold: " + (ih == null ? "null" : ih.toString()));
-        // Prism.debug("Raw slot: " + event.getRawSlot());
-        // Prism.debug("Slot: " + event.getSlot());
-        // Prism.debug("Def. Size " +
-        // event.getView().getType().getDefaultSize());
+//         Prism.debug("Raw slot: " + event.getRawSlot());
+//         Prism.debug("Slot: " + event.getSlot());
+//         Prism.debug("Def. Size " + event.getView().getType().getDefaultSize());
         // Prism.debug("Cursor Item: " + (cursoritem != null ?
         // cursoritem.getTypeId() : "null"));
         // Prism.debug("Current Item: " + (currentitem != null ?
         // currentitem.getTypeId() : "null"));
+        
+        // Double chests report 27 default size, though they actually
+        // have 6 rows of 9 for 54 slots
+        int defaultSize = event.getView().getType().getDefaultSize();
+        if( event.getInventory().getHolder() instanceof DoubleChest ){
+            defaultSize = event.getView().getType().getDefaultSize() * 2;
+        }
 
-        if( event.getSlot() == event.getRawSlot() && event.getRawSlot() <= event.getView().getType().getDefaultSize() ) {
+        // Click in the block inventory produces slot/rawslot that are equal, only until the slot numbers exceed the
+        // slot count of the inventory. At that point, they represent the player inv.
+        if( event.getSlot() == event.getRawSlot() && event.getRawSlot() <= defaultSize ) {
 
             // If BOTH items are not air then you've swapped an item. We need to
             // record an insert for the cursor item and
