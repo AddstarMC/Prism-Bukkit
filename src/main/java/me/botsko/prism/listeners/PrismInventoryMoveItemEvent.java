b3db38bd9366b1faa182d7b6d4f3d96e2db563a4
Mike Botsko <botsko@gmail.com>
2014-08-07 12:59:07 -0700

    Tracking hoppers removing items from sources

--- src/main/java/me/botsko/prism/listeners/PrismInventoryMoveItemEvent.java
+++ src/main/java/me/botsko/prism/listeners/PrismInventoryMoveItemEvent.java
@@ -20,28 +20,46 @@ public class PrismInventoryMoveItemEvent implements Listener {
      * @param event
      */
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onInventoryMoveItem(final InventoryMoveItemEvent event) {
+    public void onInventoryMoveItem(final InventoryMoveItemEvent event){
 
-        if( !Prism.getIgnore().event( "item-insert" ) )
-            return;
+        // Hopper inserted
+        if( Prism.getIgnore().event( "item-insert" ) && event.getDestination() != null ){
+            
+            // Get container
+            final InventoryHolder ih = event.getDestination().getHolder();
+            Location containerLoc = null;
+            if( ih instanceof BlockState ) {
+                final BlockState eventChest = (BlockState) ih;
+                containerLoc = eventChest.getLocation();
+            }
 
-        if( event.getDestination() == null )
-            return;
+            if( containerLoc == null )
+                return;
 
-        // Get container
-        final InventoryHolder ih = event.getDestination().getHolder();
-        Location containerLoc = null;
-        if( ih instanceof BlockState ) {
-            final BlockState eventChest = (BlockState) ih;
-            containerLoc = eventChest.getLocation();
+            if( event.getSource().getType().equals( InventoryType.HOPPER ) ) {
+                RecordingQueue.addToQueue( ActionFactory.createItemStack("item-insert", event.getItem(), event.getItem()
+                        .getAmount(), 0, null, containerLoc, "hopper") );
+            }
         }
+        
+        // Hopper removed
+        if( Prism.getIgnore().event( "item-remove" ) && event.getSource() != null ){
+            
+            // Get container
+            final InventoryHolder ih = event.getSource().getHolder();
+            Location containerLoc = null;
+            if( ih instanceof BlockState ) {
+                final BlockState eventChest = (BlockState) ih;
+                containerLoc = eventChest.getLocation();
+            }
 
-        if( containerLoc == null )
-            return;
+            if( containerLoc == null )
+                return;
 
-        if( event.getSource().getType().equals( InventoryType.HOPPER ) ) {
-            RecordingQueue.addToQueue( ActionFactory.createItemStack("item-insert", event.getItem(), event.getItem()
-                    .getAmount(), 0, null, containerLoc, "hopper") );
+            if( event.getDestination().getType().equals( InventoryType.HOPPER ) ) {
+                RecordingQueue.addToQueue( ActionFactory.createItemStack("item-remove", event.getItem(), event.getItem()
+                        .getAmount(), 0, null, containerLoc, "hopper") );
+            }
         }
     }
 }
