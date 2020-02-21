4295105ec4200495ae7061e2daa76e563f2b1476
Narimm <benjicharlton@gmail.com>
2020-02-21 19:04:45 +1000

    Fix formatting in Messenger.java
    Signed-off-by: Narimm <benjicharlton@gmail.com>

--- src/main/java/me/botsko/prism/Messenger.java
+++ src/main/java/me/botsko/prism/Messenger.java
@@ -4,99 +4,57 @@ import org.bukkit.ChatColor;
 
 public class Messenger {
 
-	/**
-	 * 
-	 */
-	protected final String plugin_name;
+    protected final String plugin_name;
 
-	/**
-	 * 
-	 * @param plugin_name
-	 */
-	public Messenger(String plugin_name) {
-		this.plugin_name = plugin_name;
-	}
+    public Messenger(String plugin_name) {
+        this.plugin_name = plugin_name;
+    }
 
-	/**
-	 * 
-	 * @param msg
-	 * @return
-	 */
-	public String playerHeaderMsg(String msg) {
-		if (msg != null) {
-			return ChatColor.LIGHT_PURPLE + plugin_name + " // " + ChatColor.WHITE + msg;
-		}
-		return "";
-	}
+    public String playerHeaderMsg(String msg) {
+        if (msg != null) {
+            return ChatColor.LIGHT_PURPLE + plugin_name + " // " + ChatColor.WHITE + msg;
+        }
+        return "";
+    }
 
-	/**
-	 * 
-	 * @param msg
-	 * @return
-	 */
-	public String playerSubduedHeaderMsg(String msg) {
-		if (msg != null) {
-			return ChatColor.LIGHT_PURPLE + plugin_name + " // " + ChatColor.GRAY + msg;
-		}
-		return "";
-	}
+    public String playerSubduedHeaderMsg(String msg) {
+        if (msg != null) {
+            return ChatColor.LIGHT_PURPLE + plugin_name + " // " + ChatColor.GRAY + msg;
+        }
+        return "";
+    }
 
-	/**
-	 * 
-	 * @param msg
-	 * @return
-	 */
-	public String playerMsg(String msg) {
-		if (msg != null) {
-			return ChatColor.WHITE + msg;
-		}
-		return "";
-	}
+    public String playerMsg(String msg) {
+        if (msg != null) {
+            return ChatColor.WHITE + msg;
+        }
+        return "";
+    }
 
-	/**
-	 * This just wraps the each message with a line end and Color White
-	 * @param msg
-	 * @return
-	 */
-	public String[] playerMsg(String[] msg) {
-		if (msg != null) {
-			for (int i = 0; i < msg.length; i++) {
-				msg[i] = playerMsg(msg[i]);
-			}
-		}
-		return msg;
-	}
+    public String[] playerMsg(String[] msg) {
+        if (msg != null) {
+            for (int i = 0; i < msg.length; i++) {
+                msg[i] = playerMsg(msg[i]);
+            }
+        }
+        return msg;
+    }
 
-	/**
-	 * 
-	 * @param cmd
-	 * @param help
-	 */
-	public String playerHelp(String cmd, String help) {
-		return ChatColor.GRAY + "/prism " + ChatColor.LIGHT_PURPLE + cmd + ChatColor.WHITE + " - " + help;
-	}
+    public String playerHelp(String cmd, String help) {
+        return ChatColor.GRAY + "/prism " + ChatColor.LIGHT_PURPLE + cmd + ChatColor.WHITE + " - " + help;
+    }
 
-	/**
-	 * 
-	 * @param msg
-	 * @return
-	 */
-	public String playerError(String msg) {
-		if (msg != null) {
-			return ChatColor.LIGHT_PURPLE + plugin_name + " // " + ChatColor.RED + msg;
-		}
-		return "";
-	}
+    public String playerError(String msg) {
+        if (msg != null) {
+            return ChatColor.LIGHT_PURPLE + plugin_name + " // " + ChatColor.RED + msg;
+        }
+        return "";
+    }
 
-	/**
-	 * 
-	 * @param msg
-	 * @return
-	 */
-	public String playerSuccess(String msg) {
-		if (msg != null) {
-			return ChatColor.LIGHT_PURPLE + plugin_name + " // " + ChatColor.GREEN + msg;
-		}
-		return "";
-	}
+    public String playerSuccess(String msg) {
+        if (msg != null) {
+            return ChatColor.LIGHT_PURPLE + plugin_name + " // " + ChatColor.GREEN + msg;
+        }
+        return "";
+    }
 }
