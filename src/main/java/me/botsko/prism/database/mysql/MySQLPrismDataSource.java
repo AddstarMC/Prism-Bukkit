51a56e540441c99cb234c4da78121c9f156e6eda
Narimm <benjicharlton@gmail.com>
2020-02-25 18:21:51 +1000

    This creates a default hikari.properties file and fills it with default values with the exception of the jbdcUrl, username and password. These values are loaded by default from the standard prism config.  However if the user sets a url, username or password in the 'hikari.properties' file it will be the default and the prism config will be ignored.  This allows users to set custom urls using varied drivers and url settings.
    Signed-off-by: Narimm <benjicharlton@gmail.com>

--- src/main/java/me/botsko/prism/database/mysql/MySQLPrismDataSource.java
+++ src/main/java/me/botsko/prism/database/mysql/MySQLPrismDataSource.java
@@ -2,24 +2,49 @@ package me.botsko.prism.database.mysql;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.util.PropertyElf;
+import me.botsko.prism.Prism;
 import me.botsko.prism.database.sql.*;
 import me.botsko.prism.database.SelectQuery;
 import org.bukkit.configuration.ConfigurationSection;
+import sun.rmi.transport.proxy.RMIDirectSocketFactory;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+import java.util.Set;
 
 /**
  * Created for use for the Add5tar MC Minecraft server
- * Created by benjamincharlton on 5/04/2019.
+ * Created by Narimm on 5/04/2019.
  */
 public class MySQLPrismDataSource extends SQLPrismDataSource {
 
     private boolean nonStandardSQL;
-    private static HikariConfig dbconfig = new HikariConfig();
+    private static HikariConfig dbConfig = new HikariConfig();
+    private static final File propFile = new File(Prism.getInstance().getDataFolder(),
+          "hikari.properties");
 
     public MySQLPrismDataSource(ConfigurationSection section) {
         super(section);
+        if(propFile.exists()){
+            Prism.log("Configuring Hikari from " +propFile.getName());
+            Prism.log("This file will not save the jdbcURL, username or password - these are loaded"
+                  + " by default from the standard prism configuration file.  If you set these "
+                  + "explicitly in the properties file the settings in the standard config will be"
+                  + "ignored.");
+            dbConfig = new HikariConfig(propFile.getPath());
+        }
         nonStandardSQL = this.section.getBoolean("useNonStandardSql", true);
         name = "mysql";
     }
+
 
     public static void updateDefaultConfig(ConfigurationSection section) {
         section.addDefault("hostname", "127.0.0.1");
@@ -29,20 +54,54 @@ public class MySQLPrismDataSource extends SQLPrismDataSource {
         section.addDefault("prefix", "prism_");
         section.addDefault("port", "3306");
         section.addDefault("useNonStandardSql", true);
+        setupDefaultProperties(section);
     }
+
+    private static void setupDefaultProperties(@Nonnull ConfigurationSection section) {
+        int maxPool = section.getInt("database.max-pool-connections", 10);
+        int minIdle = section.getInt("database.min-idle-connections", 10);
+        if (!propFile.exists()) {
+            dbConfig.addDataSourceProperty("maximumPoolSize", maxPool);
+            dbConfig.addDataSourceProperty("minimumIdle", minIdle);
+            dbConfig.setMaximumPoolSize(maxPool);
+            dbConfig.setMinimumIdle(minIdle);
+        }
+        if (!propFile.exists()) {
+            dbConfig.setPoolName("prism");
+            Properties prop = new Properties();
+            Set<String> keys = PropertyElf.getPropertyNames(HikariConfig.class);
+            for(String k:keys){
+                if(k.equals("jbdcUrl") || k.equals("username")||k.equals("password"))
+                    continue;
+                Object out  = PropertyElf.getProperty(k,dbConfig);
+                prop.setProperty(k,out.toString());
+            }
+            Properties datasourceProps = dbConfig.getDataSourceProperties();
+            for (String name:datasourceProps.stringPropertyNames()) {
+                prop.setProperty("dataSource."+name, datasourceProps.getProperty(name));
+            }
+            try {
+                OutputStream out = new FileOutputStream(propFile);
+                prop.store(out, "Prism Hikari Datasource Properties for"
+                      + " advanced database Configuration");
+                Prism.log("Database Configuration saved to - " + propFile.getPath());
+            } catch (IOException e) {
+                Prism.log("Could not save Hikari.properties - " + e.getMessage());
+            }
+        }
+    }
+
     @Override
     public MySQLPrismDataSource createDataSource() {
-        final String dns = "jdbc:mysql://" + this.section.getString("hostname") + ":"
-                + this.section.getString("port") + "/" + this.section.getString("databaseName")
-                + "?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
-
-        dbconfig.setPoolName("prism");
-        dbconfig.setMaximumPoolSize(this.section.getInt("database.max-pool-connections"));
-        dbconfig.setMinimumIdle(this.section.getInt("database.min-idle-connections"));
-        dbconfig.setJdbcUrl(dns);
-        dbconfig.setUsername(this.section.getString("username"));
-        dbconfig.setPassword(this.section.getString("password"));
-        database = new HikariDataSource(dbconfig);
+        if(dbConfig.getJdbcUrl() == null) {
+            final String dns = "jdbc:mysql://" + this.section.getString("hostname") + ":"
+                  + this.section.getString("port") + "/" + this.section.getString("databaseName")
+                  + "?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
+            dbConfig.setJdbcUrl(dns);
+            dbConfig.setUsername(this.section.getString("username"));
+            dbConfig.setPassword(this.section.getString("password"));
+        }
+        database = new HikariDataSource(dbConfig);
         createSettingsQuery();
         return this;
     }
