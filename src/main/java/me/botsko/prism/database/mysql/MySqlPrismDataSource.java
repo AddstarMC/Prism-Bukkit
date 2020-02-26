05441ac43122bbe3c66efdf5a0b55d9baaf69913
Narimm <benjicharlton@gmail.com>
2020-02-26 18:06:20 +1000

    Fix Code formatting and potential unsafe static declarations
    Signed-off-by: Narimm <benjicharlton@gmail.com>

--- src/main/java/me/botsko/prism/database/mysql/MySqlPrismDataSource.java
+++ src/main/java/me/botsko/prism/database/mysql/MySqlPrismDataSource.java
@@ -4,8 +4,9 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import com.zaxxer.hikari.util.PropertyElf;
 import me.botsko.prism.Prism;
-import me.botsko.prism.database.sql.*;
 import me.botsko.prism.database.SelectQuery;
+import me.botsko.prism.database.sql.SQLPrismDataSource;
+import me.botsko.prism.database.sql.SQLSelectQueryBuilder;
 import org.bukkit.configuration.ConfigurationSection;
 
 import javax.annotation.Nonnull;
@@ -20,28 +21,42 @@ import java.util.Set;
  * Created for use for the Add5tar MC Minecraft server
  * Created by Narimm on 5/04/2019.
  */
-public class MySQLPrismDataSource extends SQLPrismDataSource {
+public class MySqlPrismDataSource extends SQLPrismDataSource {
 
-    private boolean nonStandardSQL;
-    private static HikariConfig dbConfig = new HikariConfig();
     private static final File propFile = new File(Prism.getInstance().getDataFolder(),
           "hikari.properties");
+    private static HikariConfig dbConfig;
 
-    public MySQLPrismDataSource(ConfigurationSection section) {
-        super(section);
-        if(propFile.exists()){
-            Prism.log("Configuring Hikari from " +propFile.getName());
+    static {
+        if (propFile.exists()) {
+            Prism.log("Configuring Hikari from " + propFile.getName());
             Prism.log("This file will not save the jdbcURL, username or password - these are loaded"
                   + " by default from the standard prism configuration file.  If you set these "
                   + "explicitly in the properties file the settings in the standard config will be"
                   + "ignored.");
             dbConfig = new HikariConfig(propFile.getPath());
+        } else {
+            dbConfig = new HikariConfig();
         }
-        nonStandardSQL = this.section.getBoolean("useNonStandardSql", true);
+    }
+
+    private boolean nonStandardSql;
+
+    /**
+     * Setup the Datasource.
+     *
+     * @param section a {@link ConfigurationSection}
+     */
+    public MySqlPrismDataSource(ConfigurationSection section) {
+        super(section);
+        nonStandardSql = section.getBoolean("useNonStandardSql", true);
         name = "mysql";
     }
 
-
+    /**
+     * The adds the new requirements to an old configuration file.
+     * @param section a {@link ConfigurationSection}
+     */
     public static void updateDefaultConfig(ConfigurationSection section) {
         section.addDefault("hostname", "127.0.0.1");
         section.addDefault("username", "root");
@@ -66,15 +81,16 @@ public class MySQLPrismDataSource extends SQLPrismDataSource {
             dbConfig.setPoolName("prism");
             Properties prop = new Properties();
             Set<String> keys = PropertyElf.getPropertyNames(HikariConfig.class);
-            for(String k:keys){
-                if(k.equals("jbdcUrl") || k.equals("username")||k.equals("password"))
+            for (String k : keys) {
+                if ("jbdcUrl".equals(k) || "username".equals(k) || "password".equals(k)) {
                     continue;
-                Object out  = PropertyElf.getProperty(k,dbConfig);
-                prop.setProperty(k,out.toString());
+                }
+                Object out = PropertyElf.getProperty(k, dbConfig);
+                prop.setProperty(k, out.toString());
             }
             Properties datasourceProps = dbConfig.getDataSourceProperties();
-            for (String name:datasourceProps.stringPropertyNames()) {
-                prop.setProperty("dataSource."+name, datasourceProps.getProperty(name));
+            for (String name : datasourceProps.stringPropertyNames()) {
+                prop.setProperty("dataSource." + name, datasourceProps.getProperty(name));
             }
             try {
                 OutputStream out = new FileOutputStream(propFile);
@@ -88,8 +104,8 @@ public class MySQLPrismDataSource extends SQLPrismDataSource {
     }
 
     @Override
-    public MySQLPrismDataSource createDataSource() {
-        if(dbConfig.getJdbcUrl() == null) {
+    public MySqlPrismDataSource createDataSource() {
+        if (dbConfig.getJdbcUrl() == null) {
             final String dns = "jdbc:mysql://" + this.section.getString("hostname") + ":"
                   + this.section.getString("port") + "/" + this.section.getString("databaseName")
                   + "?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
@@ -109,7 +125,7 @@ public class MySQLPrismDataSource extends SQLPrismDataSource {
 
     @Override
     public SelectQuery createSelectQuery() {
-        if (nonStandardSQL) {
+        if (nonStandardSql) {
             return new MySQLSelectQueryBuilder(this);
         } else {
             return new SQLSelectQueryBuilder(this);
