9ef4518d9354703a10a38561303b1e3dcc5b24a9
Narimm <benjicharlton@gmail.com>
2020-02-29 00:56:59 +1000

    Remove the old PasteAPI and move paste to paste.gg - allows anon access. Pastes are set with 1hr expiry,
    Signed-off-by: Narimm <benjicharlton@gmail.com>

--- src/main/java/me/botsko/prism/commands/LookupCommand.java
+++ src/main/java/me/botsko/prism/commands/LookupCommand.java
@@ -17,130 +17,127 @@ import org.bukkit.command.CommandSender;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public class LookupCommand implements SubHandler {
 
-	/**
-	 * 
-	 */
-	private final Prism plugin;
+    /**
+     *
+     */
+    private final Prism plugin;
 
-	/**
-	 * 
-	 * @param plugin
-	 * @return
-	 */
-	public LookupCommand(Prism plugin) {
-		this.plugin = plugin;
-	}
+    /**
+     * @param plugin
+     * @return
+     */
+    public LookupCommand(Prism plugin) {
+        this.plugin = plugin;
+    }
 
-	/**
-	 * Handle the command
-	 */
-	@Override
-	public void handle(final CallInfo call) {
+    /**
+     * Handle the command
+     */
+    @Override
+    public void handle(final CallInfo call) {
 
-		// Process and validate all of the arguments
-		final QueryParameters parameters = PreprocessArgs.process(plugin, call.getSender(), call.getArgs(),
-				PrismProcessType.LOOKUP, 1, !plugin.getConfig().getBoolean("prism.queries.never-use-defaults"));
-		if (parameters == null) {
-			return;
-		}
+        // Process and validate all of the arguments
+        final QueryParameters parameters = PreprocessArgs.process(plugin, call.getSender(), call.getArgs(),
+                PrismProcessType.LOOKUP, 1, !plugin.getConfig().getBoolean("prism.queries.never-use-defaults"));
+        if (parameters == null) {
+            return;
+        }
 
-		/*
-		  Run the lookup itself in an async task so the lookup query isn't done on the
-		  main thread
-		 */
-		plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+        // Run the lookup itself in an async task so the lookup query isn't done on the main thread
+        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
 
-			// determine if defaults were used
-			final ArrayList<String> defaultsUsed = parameters.getDefaultsUsed();
-			StringBuilder defaultsReminder = new StringBuilder();
-			if (!defaultsUsed.isEmpty()) {
-				defaultsReminder.append("Using defaults:");
-				for (final String d : defaultsUsed) {
-					defaultsReminder.append(" ").append(d);
-				}
-			}
+            // determine if defaults were used
+            final ArrayList<String> defaultsUsed = parameters.getDefaultsUsed();
+            StringBuilder defaultsReminder = new StringBuilder();
+            if (!defaultsUsed.isEmpty()) {
+                defaultsReminder.append("Using defaults:");
+                for (final String d : defaultsUsed) {
+                    defaultsReminder.append(" ").append(d);
+                }
+            }
 
-			final ActionsQuery aq = new ActionsQuery(plugin);
-			final QueryResult results = aq.lookup(parameters, call.getSender());
-			StringBuilder sharingWithPlayers = new StringBuilder();
-			for (final CommandSender shareWith : parameters.getSharedPlayers()) {
-				sharingWithPlayers.append(shareWith.getName()).append(", ");
-			}
-			sharingWithPlayers = new StringBuilder(sharingWithPlayers.substring(0,
-					(sharingWithPlayers.length() == 0) ? 0 : sharingWithPlayers.length() - 2));
+            final ActionsQuery aq = new ActionsQuery(plugin);
+            final QueryResult results = aq.lookup(parameters, call.getSender());
+            StringBuilder sharingWithPlayers = new StringBuilder();
+            for (final CommandSender shareWith : parameters.getSharedPlayers()) {
+                sharingWithPlayers.append(shareWith.getName()).append(", ");
+            }
+            sharingWithPlayers = new StringBuilder(sharingWithPlayers.substring(0,
+                    (sharingWithPlayers.length() == 0) ? 0 : sharingWithPlayers.length() - 2));
 
-			// Add current sender
-			parameters.addSharedPlayer(call.getSender());
+            // Add current sender
+            parameters.addSharedPlayer(call.getSender());
 
-			for (final CommandSender player : parameters.getSharedPlayers()) {
+            for (final CommandSender player : parameters.getSharedPlayers()) {
 
-				final boolean isSender = player.getName().equals(call.getSender().getName());
+                final boolean isSender = player.getName().equals(call.getSender().getName());
 
-				if (!isSender) {
-					player.sendMessage(Prism.messenger
-							.playerHeaderMsg(ChatColor.YELLOW + "" + ChatColor.ITALIC + call.getSender().getName()
-									+ ChatColor.GOLD + " shared these Prism lookup logs with you:"));
-				} else if (sharingWithPlayers.length() > 0) {
-					player.sendMessage(
-							Prism.messenger.playerHeaderMsg(ChatColor.GOLD + "Sharing results with players: "
-									+ ChatColor.YELLOW + "" + ChatColor.ITALIC + sharingWithPlayers));
-				}
+                if (!isSender) {
+                    player.sendMessage(Prism.messenger
+                            .playerHeaderMsg(ChatColor.YELLOW + "" + ChatColor.ITALIC + call.getSender().getName()
+                                    + ChatColor.GOLD + " shared these Prism lookup logs with you:"));
+                } else if (sharingWithPlayers.length() > 0) {
+                    player.sendMessage(
+                            Prism.messenger.playerHeaderMsg(ChatColor.GOLD + "Sharing results with players: "
+                                    + ChatColor.YELLOW + "" + ChatColor.ITALIC + sharingWithPlayers));
+                }
 
-				if (!results.getActionResults().isEmpty()) {
-					player.sendMessage(Prism.messenger.playerHeaderMsg("Showing " + results.getTotalResults()
-							+ " results. Page 1 of " + results.getTotal_pages()));
-					if ((defaultsReminder.length() > 0) && isSender) {
-						player.sendMessage(Prism.messenger.playerSubduedHeaderMsg(defaultsReminder.toString()));
-					}
-					final List<Handler> paginated = results.getPaginatedActionResults();
-					if (paginated != null) {
-						int result_count = results.getIndexOfFirstResult();
-						for (final Handler a : paginated) {
-							final ActionMessage am = new ActionMessage(a);
-							if (parameters.hasFlag(Flag.EXTENDED)
-									|| plugin.getConfig().getBoolean("prism.messenger.always-show-extended")) {
-								am.showExtended();
-							}
-							am.setResultIndex(result_count);
-							MiscUtils.sendClickableTPRecord(am, player);
-							result_count++;
-						}
-						MiscUtils.sendPageButtons(results, player);
-					} else {
-						player.sendMessage(Prism.messenger
-								.playerError("Pagination can't find anything. Do you have the right page number?"));
-					}
-					if (parameters.hasFlag(Flag.PASTE)) {
-						StringBuilder paste = new StringBuilder();
-						for (final Handler a : results.getActionResults()) {
-							paste.append(new ActionMessage(a).getRawMessage()).append("\r\n");
-						}
-						player.sendMessage(MiscUtils.paste_results(plugin, paste.toString()));
-					}
-				} else {
-					if (defaultsReminder.length() > 0) {
-						if (isSender) {
-							player.sendMessage(Prism.messenger.playerSubduedHeaderMsg(defaultsReminder.toString()));
-						}
-					}
-					if (isSender) {
-						player.sendMessage(Prism.messenger.playerError("Nothing found." + ChatColor.GRAY
-								+ " Either you're missing something, or we are."));
-					}
-				}
-			}
+                if (!results.getActionResults().isEmpty()) {
+                    player.sendMessage(Prism.messenger.playerHeaderMsg("Showing " + results.getTotalResults()
+                            + " results. Page 1 of " + results.getTotal_pages()));
+                    if ((defaultsReminder.length() > 0) && isSender) {
+                        player.sendMessage(Prism.messenger.playerSubduedHeaderMsg(defaultsReminder.toString()));
+                    }
+                    final List<Handler> paginated = results.getPaginatedActionResults();
+                    if (paginated != null) {
+                        int result_count = results.getIndexOfFirstResult();
+                        for (final Handler a : paginated) {
+                            final ActionMessage am = new ActionMessage(a);
+                            if (parameters.hasFlag(Flag.EXTENDED)
+                                    || plugin.getConfig().getBoolean("prism.messenger.always-show-extended")) {
+                                am.showExtended();
+                            }
+                            am.setResultIndex(result_count);
+                            MiscUtils.sendClickableTPRecord(am, player);
+                            result_count++;
+                        }
+                        MiscUtils.sendPageButtons(results, player);
+                    } else {
+                        player.sendMessage(Prism.messenger
+                                .playerError("Pagination can't find anything. Do you have the right page number?"));
+                    }
+                    if (parameters.hasFlag(Flag.PASTE)) {
+                        StringBuilder paste = new StringBuilder();
+                        for (final Handler a : results.getActionResults()) {
+                            paste.append(new ActionMessage(a).getRawMessage()).append("\r\n");
+                        }
+                        MiscUtils.paste_results(player, results.toString());
+                    }
+                } else {
+                    if (defaultsReminder.length() > 0) {
+                        if (isSender) {
+                            player.sendMessage(Prism.messenger.playerSubduedHeaderMsg(defaultsReminder.toString()));
+                        }
+                    }
+                    if (isSender) {
+                        player.sendMessage(Prism.messenger.playerError("Nothing found." + ChatColor.GRAY
+                                + " Either you're missing something, or we are."));
+                    }
+                }
+            }
 
-			// Flush timed data
-			plugin.eventTimer.printTimeRecord();
+            // Flush timed data
+            plugin.eventTimer.printTimeRecord();
 
-		});
-	}
+        });
+    }
 
-	@Override
-	public List<String> handleComplete(CallInfo call) {
-		return PreprocessArgs.complete(call.getSender(), call.getArgs());
-	}
+    @Override
+    public List<String> handleComplete(CallInfo call) {
+        return PreprocessArgs.complete(call.getSender(), call.getArgs());
+    }
 }
--- src/main/java/me/botsko/prism/utils/MiscUtils.java
+++ src/main/java/me/botsko/prism/utils/MiscUtils.java
@@ -1,10 +1,6 @@
 package me.botsko.prism.utils;
 
 import com.google.common.base.CaseFormat;
-import com.google.common.reflect.Reflection;
-import com.helion3.pste.api.Paste;
-import com.helion3.pste.api.PsteApi;
-import com.helion3.pste.api.Results;
 import io.papermc.lib.PaperLib;
 import me.botsko.prism.Prism;
 import me.botsko.prism.actionlibs.ActionMessage;
@@ -21,234 +17,237 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.kitteh.pastegg.Paste;
+import org.kitteh.pastegg.PasteBuilder;
+import org.kitteh.pastegg.PasteContent;
+import org.kitteh.pastegg.PasteFile;
+import org.kitteh.pastegg.Visibility;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public class MiscUtils {
 
-	/**
-	 * Placing this here so it's easier to share the logic
-	 * 
-	 * @param player
-	 * @param desiredRadius
-	 * @param processType
-	 * @param config
-	 * @return
-	 */
-	public static int clampRadius(Player player, int desiredRadius, PrismProcessType processType,
-			FileConfiguration config) {
+    /**
+     * Placing this here so it's easier to share the logic.
+     *
+     * @param player        Player
+     * @param desiredRadius integer
+     * @param processType   {@link PrismProcessType}
+     * @param config        {@link FileConfiguration}
+     * @return integer
+     */
+    public static int clampRadius(Player player, int desiredRadius, PrismProcessType processType,
+                                  FileConfiguration config) {
 
-		if (desiredRadius <= 0) {
-			return config.getInt("prism.near.default-radius");
-		}
+        if (desiredRadius <= 0) {
+            return config.getInt("prism.near.default-radius");
+        }
 
-		// Safety checks for max lookup radius
-		int max_lookup_radius = config.getInt("prism.queries.max-lookup-radius");
-		if (max_lookup_radius <= 0) {
-			max_lookup_radius = 5;
-			Prism.log("Max lookup radius may not be lower than one. Using safe inputue of five.");
-		}
+        // Safety checks for max lookup radius
+        int max_lookup_radius = config.getInt("prism.queries.max-lookup-radius");
+        if (max_lookup_radius <= 0) {
+            max_lookup_radius = 5;
+            Prism.log("Max lookup radius may not be lower than one. Using safe inputue of five.");
+        }
 
-		// Safety checks for max applier radius
-		int max_applier_radius = config.getInt("prism.queries.max-applier-radius");
-		if (max_applier_radius <= 0) {
-			max_applier_radius = 5;
-			Prism.log("Max applier radius may not be lower than one. Using safe inputue of five.");
-		}
+        // Safety checks for max applier radius
+        int max_applier_radius = config.getInt("prism.queries.max-applier-radius");
+        if (max_applier_radius <= 0) {
+            max_applier_radius = 5;
+            Prism.log("Max applier radius may not be lower than one. Using safe inputue of five.");
+        }
 
-		// Does the radius exceed the configured max?
-		if (processType.equals(PrismProcessType.LOOKUP) && desiredRadius > max_lookup_radius) {
-			// If player does not have permission to override the max
-			if (player != null && !player.hasPermission("prism.override-max-lookup-radius")) {
-				return max_lookup_radius;
-			}
-			// Otherwise non-player
-			return desiredRadius;
-		}
-		else if (!processType.equals(PrismProcessType.LOOKUP) && desiredRadius > max_applier_radius) {
-			// If player does not have permission to override the max
-			if (player != null && !player.hasPermission("prism.override-max-applier-radius")) {
-				return max_applier_radius;
-			}
-			// Otherwise non-player
-			return desiredRadius;
-		}
-		else {
-			// Otherwise, the radius is valid and is not exceeding max
-			return desiredRadius;
-		}
-	}
+        // Does the radius exceed the configured max?
+        if (processType.equals(PrismProcessType.LOOKUP) && desiredRadius > max_lookup_radius) {
+            // If player does not have permission to override the max
+            if (player != null && !player.hasPermission("prism.override-max-lookup-radius")) {
+                return max_lookup_radius;
+            }
+            // Otherwise non-player
+            return desiredRadius;
+        } else if (!processType.equals(PrismProcessType.LOOKUP) && desiredRadius > max_applier_radius) {
+            // If player does not have permission to override the max
+            if (player != null && !player.hasPermission("prism.override-max-applier-radius")) {
+                return max_applier_radius;
+            }
+            // Otherwise non-player
+            return desiredRadius;
+        } else {
+            // Otherwise, the radius is valid and is not exceeding max
+            return desiredRadius;
+        }
+    }
 
-	@SuppressWarnings("unchecked")
-	public static <T extends Enum<T>> T getEnum(String from, T fallback) {
-		if (from != null)
-			try {
-				return (T) Enum.valueOf(fallback.getClass(), from.toUpperCase());
-			} catch (IllegalArgumentException ignored) {
-			}
-		return fallback;
-	}
+    @SuppressWarnings("unchecked")
+    public static <T extends Enum<T>> T getEnum(String from, T fallback) {
+        if (from != null)
+            try {
+                return (T) Enum.valueOf(fallback.getClass(), from.toUpperCase());
+            } catch (IllegalArgumentException ignored) {
+            }
+        return fallback;
+    }
 
-	public static String niceName(String in) {
-		String[] parts = in.replace('_', ' ').trim().split("", 2);
-		return parts[0].toUpperCase() + parts[1].toLowerCase();
-	}
+    public static String niceName(String in) {
+        String[] parts = in.replace('_', ' ').trim().split("", 2);
+        return parts[0].toUpperCase() + parts[1].toLowerCase();
+    }
 
-	public static String niceLower(String in) {
-		return in.replace('_', ' ').trim().toLowerCase();
-	}
+    public static String niceLower(String in) {
+        return in.replace('_', ' ').trim().toLowerCase();
+    }
 
-	/**
-	 * 
-	 * @param prism
-	 * @param results
-	 * @return
-	 */
-	public static String paste_results(Prism prism, String results) {
+    /**
+     * Paste results and return a web address.
+     * Async Method.
+     *
+     * @param sender  CommandSender.
+     * @param results String
+     */
+    public static void paste_results(CommandSender sender, String results) {
 
-		final String prismWebUrl = "https://pste.me/";
+        final String prismWebUrl = "https://paste.gg/";
 
-		if (!prism.getConfig().getBoolean("prism.paste.enable")) {
-			return Prism.messenger.playerError("PSTE.me paste bin support is currently disabled by config.");
-		}
+        if (!Prism.getInstance().getConfig().getBoolean("prism.paste.enable")) {
+            sender.sendMessage(Prism.messenger.playerError(
+                    "Paste.gg support is currently disabled by config."));
+        }
 
-		final String apiUsername = prism.getConfig().getString("prism.paste.username");
-		final String apiKey = prism.getConfig().getString("prism.paste.api-key");
+        int expire = 60 * 60 * 1000;
+        PasteFile file = new PasteFile("Prism Result",
+                new PasteContent(PasteContent.ContentType.TEXT, results));
 
-		if (!apiKey.matches("[0-9a-z]+")) {
-			return Prism.messenger.playerError("Invalid API key.");
-		}
+        final PasteBuilder.PasteResult result
+                = new PasteBuilder().name("Prism Results")
+                .expireIn(expire)
+                .addFile(file)
+                .visibility(Visibility.UNLISTED)
+                .build();
+        if (result.getPaste().isPresent()) {
+            Paste paste = result.getPaste().get();
+            sender.sendMessage(Prism.messenger.playerSuccess("Successfully pasted results: "
+                    + prismWebUrl
+                    + paste.getId()));
+        } else {
+            sender.sendMessage(Prism.messenger.playerError(
+                    "Unable to paste results (" + ChatColor.YELLOW + result.getMessage()
+                            + ChatColor.RED + ")."));
 
-		final PsteApi api = new PsteApi(apiUsername, apiKey);
+        }
 
-		try {
+    }
 
-			final Paste paste = new Paste();
-			paste.setPaste(results);
+    public static void sendClickableTPRecord(ActionMessage a, CommandSender player) {
+        boolean isSpigot = false;
+        boolean isPaper = PaperLib.isPaper();
+        try {
+            Player.class.getMethod("spigot");
+            isSpigot = true;
+        } catch (NoSuchMethodException ignored) {
+        }
+        if (isPaper || isSpigot) {
+            String[] message = Prism.messenger.playerMsg(a.getMessage());
+            //line 1 holds the index so we set that as the highlighted for command click
+            final List<BaseComponent> toSend = new ArrayList<>();
+            int i = 0;
+            for (String m : message) {
+                BaseComponent[] text = TextComponent.fromLegacyText((player instanceof Player) ? m : m.replace("\n", ""));
+                if (i == 0) {
+                    Arrays.asList(text).forEach(baseComponent -> {
+                        baseComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+                                new TextComponent[]{new TextComponent("Click to teleport")}));
+                        baseComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/pr tp "
+                                + a.getIndex()));
+                        toSend.add(baseComponent);
+                    });
+                } else {
+                    toSend.addAll(Arrays.asList(text));
+                }
+                i++;
+            }
+            BaseComponent[] finalMessage = new BaseComponent[toSend.size()];
+            toSend.toArray(finalMessage);
+            if (PaperLib.isPaper()) {
+                player.sendMessage(finalMessage);
+            } else {
+                player.spigot().sendMessage(finalMessage);
+            }
+        } else {
+            player.sendMessage(Prism.messenger.playerMsg(a.getMessage()));
+        }
+    }
 
-			final Results response = api.createPaste(paste);
-			return Prism.messenger.playerSuccess(
-					"Successfully pasted results: " + prismWebUrl + "#/" + response.getResults().getSlug());
+    public static void sendPageButtons(QueryResult results, CommandSender player) {
+        if (player instanceof Player) {
+            if (PaperLib.isPaper()) {
+                if (results.getPage() == 1) {
+                    if (results.getTotal_pages() > 1)
+                        player.sendMessage(MiscUtils.getNextButton());
+                } else if (results.getPage() < results.getTotal_pages())
+                    player.sendMessage(MiscUtils.getPrevNextButtons());
+                else if (results.getPage() == results.getTotal_pages())
+                    player.sendMessage(MiscUtils.getPreviousButton());
+            } else {
+                if (results.getPage() == 1) {
+                    if (results.getTotal_pages() > 1)
+                        player.spigot().sendMessage(MiscUtils.getNextButton());
+                } else if (results.getPage() < results.getTotal_pages())
+                    player.spigot().sendMessage(MiscUtils.getPrevNextButtons());
+                else if (results.getPage() == results.getTotal_pages())
+                    player.spigot().sendMessage(MiscUtils.getPreviousButton());
+            }
+        }
+    }
 
-		}
-		catch (final Exception up) {
-			Prism.debug(up.toString());
-			return Prism.messenger.playerError(
-					"Unable to paste results (" + ChatColor.YELLOW + up.getMessage() + ChatColor.RED + ").");
-		}
-	}
-	public static void sendClickableTPRecord(ActionMessage a, CommandSender player){
-		boolean isSpigot = false;
-		boolean isPaper = PaperLib.isPaper();
-		try {
-			Player.class.getMethod("spigot");
-			isSpigot = true;
-		} catch (NoSuchMethodException ignored){
-		}
-		if (isPaper || isSpigot) {
-			String[] message = Prism.messenger.playerMsg(a.getMessage());
-			//line 1 holds the index so we set that as the highlighted for command click
-			final List<BaseComponent> toSend = new ArrayList<>();
-			int i = 0;
-			for (String m : message) {
-				BaseComponent[] text = TextComponent.fromLegacyText((player instanceof Player) ? m : m.replace("\n", ""));
-				if (i == 0) {
-					Arrays.asList(text).forEach(baseComponent -> {
-						baseComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-							new TextComponent[]{new TextComponent("Click to teleport")}));
-						baseComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/pr tp "
-							+ a.getIndex()));
-						toSend.add(baseComponent);
-					});
-				}else{
-					toSend.addAll(Arrays.asList(text));
-				}
-				i++;
-			}
-			BaseComponent[] finalMessage = new BaseComponent[toSend.size()];
-			toSend.toArray(finalMessage);
-			if(PaperLib.isPaper()) {
-				player.sendMessage(finalMessage);
-			}else{
-				player.spigot().sendMessage(finalMessage);
-			}
-		}else{
-			player.sendMessage(Prism.messenger.playerMsg(a.getMessage()));
-		}
-	}
-	public static void sendPageButtons(QueryResult results, CommandSender player){
-		if (player instanceof Player) {
-			if (PaperLib.isPaper()) {
-				if (results.getPage() == 1) {
-					if (results.getTotal_pages() > 1)
-						player.sendMessage(MiscUtils.getNextButton());
-				}
-				else if (results.getPage() < results.getTotal_pages())
-					player.sendMessage(MiscUtils.getPrevNextButtons());
-				else if (results.getPage() == results.getTotal_pages())
-					player.sendMessage(MiscUtils.getPreviousButton());
-			} else {
-				if (results.getPage() == 1) {
-					if (results.getTotal_pages() > 1)
-						player.spigot().sendMessage(MiscUtils.getNextButton());
-				}
-				else if (results.getPage() < results.getTotal_pages())
-					player.spigot().sendMessage(MiscUtils.getPrevNextButtons());
-				else if (results.getPage() == results.getTotal_pages())
-					player.spigot().sendMessage(MiscUtils.getPreviousButton());
-			}
-		}
-	}
+    public static List<String> getStartingWith(String start, Iterable<String> options, boolean caseSensitive) {
+        final List<String> result = new ArrayList<>();
+        if (caseSensitive) {
+            for (final String option : options) {
+                if (option.startsWith(start))
+                    result.add(option);
+            }
+        } else {
+            start = start.toLowerCase();
+            for (final String option : options) {
+                if (option.toLowerCase().startsWith(start))
+                    result.add(option);
+            }
+        }
 
-	public static List<String> getStartingWith(String start, Iterable<String> options, boolean caseSensitive) {
-		final List<String> result = new ArrayList<>();
-		if (caseSensitive) {
-			for (final String option : options) {
-				if (option.startsWith(start))
-					result.add(option);
-			}
-		}
-		else {
-			start = start.toLowerCase();
-			for (final String option : options) {
-				if (option.toLowerCase().startsWith(start))
-					result.add(option);
-			}
-		}
+        return result;
+    }
 
-		return result;
-	}
+    public static List<String> getStartingWith(String arg, Iterable<String> options) {
+        return getStartingWith(arg, options, true);
+    }
 
-	public static List<String> getStartingWith(String arg, Iterable<String> options) {
-		return getStartingWith(arg, options, true);
-	}
+    public static void dispatchAlert(String msg, List<String> commands) {
+        String colorized = TypeUtils.colorize(msg);
+        String stripped = ChatColor.stripColor(colorized);
+        for (String command : commands) {
+            if (command.equals("examplecommand <alert>"))
+                continue;
+            String processedCommand = command.replace("<alert>", stripped);
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), processedCommand);
+        }
+    }
 
-	public static void dispatchAlert(String msg, List<String> commands) {
-		String colorized = TypeUtils.colorize(msg);
-		String stripped = ChatColor.stripColor(colorized);
-		for (String command : commands) {
-			if (command.equals("examplecommand <alert>"))
-				continue;
-			String processedCommand = command.replace("<alert>", stripped);
-			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), processedCommand);
-		}
-	}
-
-	public static String getEntityName(Entity entity) {
-		if (entity == null)
-			return "unknown";
-		if (entity.getType() == EntityType.PLAYER)
-			return entity.getName();
-		return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, entity.getType().name());
-	}
+    public static String getEntityName(Entity entity) {
+        if (entity == null)
+            return "unknown";
+        if (entity.getType() == EntityType.PLAYER)
+            return entity.getName();
+        return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, entity.getType().name());
+    }
 
     public static TextComponent getPreviousButton() {
         TextComponent textComponent = new TextComponent(" [<< Prev]");
         textComponent.setColor(ChatColor.GRAY);
         textComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponent[]{
-				new TextComponent("Click to view the previous page")}));
+                new TextComponent("Click to view the previous page")}));
         textComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/pr pg p"));
         return textComponent;
 
