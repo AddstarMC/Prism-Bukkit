88f6570eaa3d9d7b8a6a8fbacd9180763ab5a159
NeverGrowUp <nevergrowup71@gmail.com>
2021-06-23 18:56:28 +0930

    Fixes startup check to make sure tables exist. Previously if "xxx-data_extra" was already defined in another database/schema, then it wouldnt create another in the new schema.

--- Prism/src/main/java/me/botsko/prism/database/sql/SqlPrismDataSource.java
+++ Prism/src/main/java/me/botsko/prism/database/sql/SqlPrismDataSource.java
@@ -182,7 +182,12 @@ public abstract class SqlPrismDataSource implements PrismDataSource {
             // extra prism data table (check if it exists first, so we can avoid
             // re-adding foreign key stuff)
             final DatabaseMetaData metadata = conn.getMetaData();
-            ResultSet resultSet = metadata.getTables(conn.getCatalog(), conn.getSchema(), prefix + "data_extra", new String[]{"TABLE"});
+            ResultSet resultSet = metadata.getTables(
+                    conn.getCatalog(),
+                    conn.getSchema(),
+                    prefix + "data_extra",
+                    new String[]{"TABLE"}
+            );
             if (!resultSet.next()) {
 
                 // extra data
