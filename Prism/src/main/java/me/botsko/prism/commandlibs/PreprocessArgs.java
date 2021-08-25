3a79b31471958ab432cb0930ff0f0dd25288d34e
Narimm <benjicharlton@gmail.com>
2021-08-25 14:30:59 +1000

    Improve flag handling to return not found if it doesnt exist as oppose to a permission failure.
    Signed-off-by: Narimm <benjicharlton@gmail.com>

--- Prism/src/main/java/me/botsko/prism/commandlibs/PreprocessArgs.java
+++ Prism/src/main/java/me/botsko/prism/commandlibs/PreprocessArgs.java
@@ -10,7 +10,6 @@ import me.botsko.prism.parameters.PrismParameterHandler;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 
 import java.util.ArrayList;
 import java.util.Collection;
